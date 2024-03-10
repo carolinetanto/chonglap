cf1969c161dc57a0eff88d843fc1509ecdf1710a
Caroline Avery Tanto <75901018+carolinetanto@users.noreply.github.com>
2024-03-10 08:51:41 +0800

    navbar

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -30,7 +30,7 @@ export const Navbar = () => {
               </div>
             </div>
             <div className="md:hidden flex items-center">
-              <button className="inline-flex items-center justify-center p-2 rounded-md text-white hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" onClick={toggleNavbar}>
+              <button className="inline-flex items-center justify-center p-2 rounded-md text-white hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" onClick = {()=> toggleNavbar()}>
                 {isClick ? (
                   <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12"/>
