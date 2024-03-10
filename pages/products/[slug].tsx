441634cd1ac4ff40cf463a73440c99e8cb3b39fc
Caroline Avery Tanto <75901018+carolinetanto@users.noreply.github.com>
2024-03-10 19:37:02 +0800

    Store

--- pages/products/[slug].tsx
+++ pages/products/[slug].tsx
@@ -2,9 +2,11 @@ import { GetStaticProps } from "next";
 import { GetStaticPaths } from "next";
 import { useState } from "react";
 import Gallery  from "@/components/Gallery";
-
 import { getProducts, getProductBySlug } from "@/lib/service";
-
+import Accordion from '@mui/material/Accordion';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 export default function ProductDetails({ product }: { product: any }) {
   const imageCount = product.productField.gallery.nodes.length;
   const [headerImage, setHeaderImage] = useState(
@@ -42,7 +44,47 @@ export default function ProductDetails({ product }: { product: any }) {
         className="product-content w-full md:w-3/5 mx-auto mt-20 py-6 text-lg"
         dangerouslySetInnerHTML={{ __html: product.productField.description }}
       ></div>
-      <div className="product-store"></div>
+      <div className="product-store">
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1-content"
+          id="panel1-header"
+        >
+          Accordion 1
+        </AccordionSummary>
+        <AccordionDetails>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          malesuada lacus ex, sit amet blandit leo lobortis eget.
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2-content"
+          id="panel2-header"
+        >
+          Accordion 2
+        </AccordionSummary>
+        <AccordionDetails>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          malesuada lacus ex, sit amet blandit leo lobortis eget.
+        </AccordionDetails>
+      </Accordion>
+      <Accordion defaultExpanded>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel3-content"
+          id="panel3-header"
+        >
+          Accordion Actions
+        </AccordionSummary>
+        <AccordionDetails>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          malesuada lacus ex, sit amet blandit leo lobortis eget.
+        </AccordionDetails>
+      </Accordion>
+      </div>
       <div className="product-brand w-full md:w-3/5 mx-auto mt-20 py-6 text-lg">
         <p>Product by</p>
         <img src={product.productField.brand.node.sourceUrl} className="max-h-[100px]"/>
