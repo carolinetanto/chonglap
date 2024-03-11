e132e8ab06ef47dc1edb9629e8006f5590cf0fe8
Caroline Avery Tanto <75901018+carolinetanto@users.noreply.github.com>
2024-03-11 18:34:53 +0800

    Accordion updated

--- pages/products/[slug].tsx
+++ pages/products/[slug].tsx
@@ -7,6 +7,7 @@ import Accordion from '@mui/material/Accordion';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
 export default function ProductDetails({ product }: { product: any }) {
   const imageCount = product.productField.gallery.nodes.length;
   const [headerImage, setHeaderImage] = useState(
@@ -44,46 +45,64 @@ export default function ProductDetails({ product }: { product: any }) {
         className="product-content w-full md:w-3/5 mx-auto mt-20 py-6 text-lg"
         dangerouslySetInnerHTML={{ __html: product.productField.description }}
       ></div>
-      <div className="product-store">
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1-content"
-          id="panel1-header"
-        >
-          Accordion 1
-        </AccordionSummary>
-        <AccordionDetails>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          malesuada lacus ex, sit amet blandit leo lobortis eget.
-        </AccordionDetails>
-      </Accordion>
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel2-content"
-          id="panel2-header"
-        >
-          Accordion 2
-        </AccordionSummary>
-        <AccordionDetails>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          malesuada lacus ex, sit amet blandit leo lobortis eget.
-        </AccordionDetails>
-      </Accordion>
-      <Accordion defaultExpanded>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel3-content"
-          id="panel3-header"
-        >
-          Accordion Actions
-        </AccordionSummary>
-        <AccordionDetails>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          malesuada lacus ex, sit amet blandit leo lobortis eget.
-        </AccordionDetails>
-      </Accordion>
+      <div className="product-store w-full md:w-3/5 mx-auto mt-20 py-6 text-lg">
+        {product.productField.store.hongKongIsland && (
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1-content"
+              id="panel1-header"
+            >
+              Hong Kong Island
+            </AccordionSummary>
+            <AccordionDetails>
+              {product.productField.store.hongKongIsland.map((store: any, index: any) => (
+                <div key={`hki-${index}`}>
+                  <h5>{store.name}</h5>
+                  <p>{store.address}</p>
+                </div>
+              ))}
+            </AccordionDetails>
+          </Accordion>
+        )}
+        {product.productField.store.kowloon && (
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel2-content"
+              id="panel2-header"
+            >
+              Kowloon
+            </AccordionSummary>
+            <AccordionDetails>
+              {product.productField.store.kowloon.map((store: any, index: any) => (
+                <div key={`hki-${index}`}>
+                  <h5>{store.name}</h5>
+                  <p>{store.address}</p>
+                </div>
+              ))}
+            </AccordionDetails>
+          </Accordion>
+        )}
+        {product.productField.store.newTerritories && (
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1-content"
+              id="panel1-header"
+            >
+              New Territories
+            </AccordionSummary>
+            <AccordionDetails>
+              {product.productField.store.newTerritories.map((store: any, index: any) => (
+                <div key={`hki-${index}`}>
+                  <h5>{store.name}</h5>
+                  <p>{store.address}</p>
+                </div>
+              ))}
+            </AccordionDetails>
+          </Accordion>
+        )}
       </div>
       <div className="product-brand w-full md:w-3/5 mx-auto mt-20 py-6 text-lg">
         <p>Product by</p>
