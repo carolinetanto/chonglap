534559f6162dfcce2d7fd6bc6e287ff9ad3352bb
Caroline Avery Tanto <75901018+carolinetanto@users.noreply.github.com>
2024-03-13 02:59:33 +0800

    remove mesh animation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,6 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Navbar } from "@/components/Navbar";
 import { Footer } from "@/components/Footer";
-import { Canvas } from "@react-three/fiber";
-import AnimatedMeshBackground from "@/components/AnimatedMeshBackground";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
