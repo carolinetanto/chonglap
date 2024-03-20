7932db501bef318c043eb2ca4dbbfa659e6fee4c
Caroline Avery Tanto <75901018+carolinetanto@users.noreply.github.com>
2024-03-20 08:18:53 +0800

    pages

--- pages/taiwan-branch/index.tsx
+++ pages/taiwan-branch/index.tsx
@@ -21,7 +21,7 @@ const index = () => {
         <div className="taiwan-content mt-5">
           <h1 className="text text-5xl">Company profile</h1>
           <br></br>
-          <p className='address'>Chuangkang Medical Equipment Co., Ltd. Founded in 2012, its parent company is Creation (Hong Kong) Co., Ltd. Since its establishment, Chuangkang has been committed to promoting public health and improving people's quality of life, and is committed to introducing innovative medical technologies and providing high-quality and efficient medical devices. Through unremitting efforts, it has become a core enterprise in the field of medical diagnostic product distribution in Taiwan. Chuangkang adheres to the marketing strategy of professional academic promotion and focuses on participating in various high-level medical academic conferences. Through cooperation with large scientific research institutions, universities and well-known enterprises, it actively promotes local clinical scientific research and forms a unique core Competitiveness. At present, the company has a well-trained professional, formalized marketing and technical support team, and has established a promotion and sales network throughout Taiwan with public and private hospitals and medical diagnostic institutions as the main channels.</p>
+          <p className="address">Chuangkang Medical Equipment Co., Ltd. Founded in 2012, its parent company is Creation (Hong Kong) Co., Ltd. Since its establishment, Chuangkang has been committed to promoting public health and improving people's quality of life, and is committed to introducing innovative medical technologies and providing high-quality and efficient medical devices. Through unremitting efforts, it has become a core enterprise in the field of medical diagnostic product distribution in Taiwan. Chuangkang adheres to the marketing strategy of professional academic promotion and focuses on participating in various high-level medical academic conferences. Through cooperation with large scientific research institutions, universities and well-known enterprises, it actively promotes local clinical scientific research and forms a unique core Competitiveness. At present, the company has a well-trained professional, formalized marketing and technical support team, and has established a promotion and sales network throughout Taiwan with public and private hospitals and medical diagnostic institutions as the main channels.</p>
         </div>
       </div>
       <div className="spliter justify-center w-full min-h-[200px]" style={{
@@ -32,7 +32,7 @@ const index = () => {
       <div className="mx-auto py-8 px-8 object-strategy grid grid-cols-2 gap-6">
         <div className="objective">
           <h1 className="text text-3xl lg:text-5xl">Objective</h1>
-          <ul className='mt-5'>
+          <ul className="mt-5">
             <li><p>Expand service network and strengthen existing business partnerships</p></li>
             <li><p>Recognize gaps in the market and develop innovative ideas to seize market opportunities</p></li>
             <li><p>Understand customer needs and provide efficient and professional services to meet customer needs</p></li>
@@ -40,8 +40,8 @@ const index = () => {
         </div>
         <div className="strategy">
           <h1 className="text text-3xl lg:text-5xl">Strategy</h1>
-          <ul className='mt-5'>
-            <li><p>Respect customers’ different perspectives and promote cultural communication between local and overseas countries in the healthcare field</p></li>
+          <ul className="mt-5">
+            <li><p>Respect customers different perspectives and promote cultural communication between local and overseas countries in the healthcare field</p></li>
             <li><p>Use network connections to increase investment opportunities</p></li>
             <li><p>Develop marketing strategies such as sponsorship and participation in academic exchanges, and create and strengthen business ties with relevant international fields</p></li>
           </ul>
